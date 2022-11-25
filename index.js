fd1a0941805258b2de530cd26d03f68f0944af9c
GladysKigen <gladys.kigen@student.moringa.com>
2022-11-25 21:23:00 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(spy){
+    return spy()
+}
+function returnsANamedFunction(){
+    return function Moringa(){
+        return 0;
+    }
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    }
+}
